4913bb64401af9234ba18fe8d5d8d0d8cafc1d81
Liav Koren <liav@vmfarms.com>
2018-04-04 08:52:00 -0400

    A bunch of cleanup.

--- cartpole_value_iteration_discrete.py
+++ cartpole_value_iteration_discrete.py
@@ -1,78 +1,81 @@
 from itertools import product
 from math import radians
+from random import random
 
 import gym
+import numpy as np
 
 from quantizer import Quantizer
 
-simulator = None
-quant = Quantizer
+
+class Simulator:
+    def __init__(self):
+        self.engine = gym.make('CartPole-v1')
+
+    def step(self, state, action):
+        self.engine.reset()
+        self.engine.env.state = state
+        return self.engine.step(action)
 
 
-def step(state, action):
-    """
-    reset
-    env.env.state = state
-    action
-    """
-    global simulator
-    if simulator is None:
-        simulator = gym.make('CartPole-v1')
-    simulator.reset()
-    simulator.env.state = state
-    # <np.array(state)>, reward, done, {}
-    return simulator.step(action)
+def init_state_values(buckets):
+    """ Return dict of {<state tuples>: 0}. <State tuple> is a quantized label for an observation. """
+    state_tuples = []
+    ranges = [list(range(bucket)) for bucket in buckets]
+    for tup in product(*ranges):
+        state_tuples.append(tup)
+    return {tup: 0 for tup in state_tuples}
 
 
-def value_iteration(gamma=.95):
-    """ Return a dict of tuple to value. """
-    theta = 10e-4
-    values = init_state_values()
-    gamma = .95
+def value_iteration(quantizer, gamma=.95, theta=10e-4):
+    """ Return a dict of state tuples to values. """
+    values = init_state_values(quantizer.buckets)
+    simulator = Simulator()
     while True:
         delta = 0
         for quantized_state, old_value in values.items():
-            actual_state = dequantize_state(quantized_state)
+            actual_state = quantizer.dequantize(quantized_state)
             best_reward = float('-inf')
             for action in [0, 1]:
-                new_state, reward, done, _ = step(actual_state, action)
+                new_state, reward, done, _ = simulator.step(actual_state, action)
                 if done:
                     reward = 0
                 if reward > best_reward:
                     best_reward = reward
-
-            values[quantized_state] = best_reward + gamma * values[state2state_number(new_state)]
+            values[quantized_state] = best_reward + gamma * values[quantizer.quantize(new_state)]
             delta = max(delta, abs(old_value - values[quantized_state]))
-        print(delta)
+        print(f'Computing value function, delta is: {delta}', end='\r', flush=True)
         if delta < theta:
             print(values)
             return values
 
 
-def policy(env, values, state, gamma=.95):
-    best_action = -1
-    best_reward = float('-inf')
+def policy(values, state, quantizer, simulator, gamma=.95):
+    rewards = []
     for action in [0, 1]:
-        new_state, reward, _, _ = step(state, action)
-        expected_reward = reward + gamma * values[state2state_number(new_state)]
-        if expected_reward > best_reward:
-            best_reward = expected_reward
-            best_action = action
-    return best_action
+        new_state, reward, _, _ = simulator.step(state, action)
+        expected_reward = reward + gamma * values[quantizer.quantize(new_state)]
+        rewards.append(expected_reward)
+    no_difference = rewards[0] == rewards[1]
+    if no_difference:
+        # random tie-breaking
+        return random() > 0.5
+    else:
+        return np.argmax(rewards)
 
 
-def go(env):
+def go(env, quantizer):
     best_score = float('-inf')
-    gamma = .99
-    # starting_state = env.reset()
-    values = value_iteration(gamma=gamma)
+    gamma = .95
+    values = value_iteration(quantizer, gamma=gamma, theta=.005)
+    simulator = Simulator()
     for episode in range(20):
         state = env.reset()
-        action = policy(env, values, state, gamma=gamma)
+        action = policy(values, state, quantizer, simulator, gamma=gamma)
         score = 0
         for t in range(1000):
             env.render()
-            action = policy(env, values, state, gamma=gamma)
+            action = policy(values, state, quantizer, simulator, gamma=gamma)
             state, reward, done, info = env.step(action)
             score += reward
             if done:
@@ -82,49 +85,15 @@ def go(env):
                 break
 
 
-def state2state_number(state):
-    global quant
-    return quant.quantize(state)
-
-
-def tuples():
-    out = []
-    global buckets
-    ranges = [list(range(bucket)) for bucket in buckets]
-    for tup in product(*ranges):
-        out.append(tup)
-    return out
-
-
-def init_state_values():
-    state_tuples = tuples()
-    return {tup: 0 for tup in state_tuples}
-
-
-state_map = None
-
-
-def dequantize_state(state_tuple):
-    global quant
-    return quant.dequantize(state_tuple)
-
-
 if __name__ == '__main__':
     env = gym.make('CartPole-v1')
-
-    """
-    bucket_size = 6
-    buckets = [bucket_size] * 4
-    """
-    buckets = [20, 6, 6, 6]
+    buckets = [10, 6, 6, 6]
     low = env.observation_space.low
-    high = env.observation_space.high
     low[1] = -0.5
     low[3] = -radians(50)
+    high = env.observation_space.high
     high[1] = 0.5
     high[3] = radians(50)
 
-    quant = Quantizer(low=low, high=high, buckets=buckets)
-
-    go(env)
-    print(buckets)
+    quantizer = Quantizer(low=low, high=high, buckets=buckets)
+    go(env, quantizer)
